2392e315dcd68c6552768cd4ef4fe8fd43762822
Zhixiong Lu <zhixiong.lu@qq.com>
2024-11-28 22:24:26 +0800

    feat: basic lexer/scanner

--- jlox/src/main/java/com/chabao18/interpreter/jlox/Lox.java
+++ jlox/src/main/java/com/chabao18/interpreter/jlox/Lox.java
@@ -2,12 +2,11 @@ package com.chabao18.interpreter.jlox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.List;
 
 public class Lox {
     public static void main(String[] args) throws IOException {
@@ -26,17 +25,21 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
-        if (hadError) System.exit(65);
+        if (hadError) {
+            System.exit(65);
+        }
     }
 
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        for (;;) {
-            System.out.println("> ");
+        for (; ; ) {
+            System.out.print("> ");
             String line = reader.readLine();
-            if (line == null) break;
+            if (line == null) {
+                break;
+            }
             run(line);
             // won't interrupt the program even if there is an error
             hadError = false;
@@ -47,19 +50,19 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (token : tokens)
+        for (Token token : tokens) {
             System.out.println(token);
+        }
     }
 
-    static void error(int line, String message) {
+    static void error(int line, String format, Object... args) {
+        String message = String.format(format, args);
         report(line, "", message);
     }
 
     // TODO 指出具体错误位置
-    private static void report(int line, String where,
-                               String message) {
-        System.err.println(
-                "[line " + line + "] Error" + where + ": " + message);
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 }
