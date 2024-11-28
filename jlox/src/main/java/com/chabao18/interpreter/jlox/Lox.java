ee1c46cf7ae1a27c6ecbbdbe28412f9aeb65915a
Zhixiong Lu <zhixiong.lu@qq.com>
2024-11-28 21:09:54 +0800

    feat: error handling

--- jlox/src/main/java/com/chabao18/interpreter/jlox/Lox.java
+++ jlox/src/main/java/com/chabao18/interpreter/jlox/Lox.java
@@ -21,9 +21,12 @@ public class Lox {
         }
     }
 
+    static boolean hadError = false;
+
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -35,6 +38,8 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            // won't interrupt the program even if there is an error
+            hadError = false;
         }
     }
 
@@ -44,5 +49,17 @@ public class Lox {
 
         for (token : tokens)
             System.out.println(token);
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    // TODO 指出具体错误位置
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
